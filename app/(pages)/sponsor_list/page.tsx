4752bf74bae1036740de3da834d338cfa646d0fb
tiassousylvain6120 <tiassousylvain6120@gmail.com>
2024-07-17 16:20:53 +0000

    sponsor list

--- app/(pages)/sponsor_list/page.tsx
+++ app/(pages)/sponsor_list/page.tsx
@@ -20,18 +20,20 @@ import { ParticipantModel } from "@/models/participantModel";
 import { Search } from "lucide-react";
 import { useEffect, useState } from "react";
 import Image from "next/image";
+import { SponsorModel } from "@/models/SponsorModel";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 
 export default function ParticipantList() {
-    const [data, setData] = useState<ParticipantModel[]>([]);
+    const [data, setData] = useState<SponsorModel[]>([]);
     const [query, setQuery] = useState('');
-    const [results, setResults] = useState<ParticipantModel[]>([]);
+    const [results, setResults] = useState<SponsorModel[]>([]);
     const pathname = usePathname();
 
     useEffect(() => {
-        Api.read('/api/participant').then((value) => {
+        Api.read('/api/sponsor').then((value) => {
             setData(value);
+            console.log(value)
         })
     }, []);
 
@@ -41,7 +43,7 @@ export default function ParticipantList() {
             setQuery(searchTerm);
             // Simulation d'une recherche avec un tableau de données statique
             const filteredResults = data.filter(item =>
-                item.firstName.toLowerCase().includes(searchTerm.toLowerCase())
+                item.organisationName.toLowerCase().includes(searchTerm.toLowerCase())
             );
             setResults(filteredResults);
         }
@@ -54,17 +56,17 @@ export default function ParticipantList() {
 
     //tableau
 
-    const tableConstruction = (data: ParticipantModel[]) => {
+    const tableConstruction = (data: SponsorModel[]) => {
         return data.map((items) => (
             <TableRow key={items.id}>
                 <TableCell className="">
 
                     <HoverCard>
                         <HoverCardTrigger className="flex items-center justify-center rounded-full">
-                            <Image src={items.capture} alt="" width="40" height="40" className="object-cover" />
+                            <Image src={items.logoUrl} alt="" width="80" height="80" className="object-cover" />
                         </HoverCardTrigger>
                         <HoverCardContent>
-                            <Image src={items.capture} alt="" width="240" height="240" className="object-cover" />
+                            <Image src={items.captureUrl} alt="" width="240" height="240" className="object-cover" />
                         </HoverCardContent>
                     </HoverCard>
 
@@ -72,6 +74,7 @@ export default function ParticipantList() {
                 </TableCell>
 
                 <TableCell className="font-medium">{items.firstName} {items.lastName}</TableCell>
+                <TableCell className="">{items.organisationName}</TableCell>
                 <TableCell className="">{items.email}</TableCell>
                 <TableCell className="">{items.phone}</TableCell>
                 <TableCell className="text-right">{items.reference}</TableCell>
@@ -84,11 +87,11 @@ export default function ParticipantList() {
             <div className="felx flex-col space-y-20">
 
                 <div className="flex space-x-10">
-                    <Link href="/participantList" className={pathname == "/participantList" ? "text-blue-600 font-bold" : ""}>
+                    <Link href="/participantList" className={pathname == "/participantList"? "text-blue-600 font-bold": ""}>
                         Liste des Participants
                     </Link>
 
-                    <Link href="/sponsor_list" className={pathname == "/sponsor_list" ? "text-blue-600 font-bold" : ""}>
+                    <Link href="/sponsor_list" className={pathname == "/sponsor_list"? "text-blue-600 font-bold": ""}>
                         Liste des Sponsors
                     </Link>
                 </div>
@@ -96,7 +99,7 @@ export default function ParticipantList() {
                 <div className="flex items-center justify-center my-5">
                     <div className="flex space-x-3 items-center">
                         <Input className="md:w-[300px] w-[250px] "
-                            placeholder={"Recherche par Nom"}
+                            placeholder={"Recherche par le nom de l'entreprise"}
                             value={query}
                             onChange={(e: any) => searchFunction(e)}
                         />
@@ -106,11 +109,12 @@ export default function ParticipantList() {
                 </div>
                 {/*table*/}
                 <Table>
-                    <TableCaption>Liste des participants.</TableCaption>
+                    <TableCaption>Liste des sponsors.</TableCaption>
                     <TableHeader>
                         <TableRow>
-                            <TableHead className="">Capture de dépot</TableHead>
+                            <TableHead className="">Logo de la société</TableHead>
                             <TableHead className="">Nom prénom</TableHead>
+                            <TableHead className="">Nom De la societé</TableHead>
                             <TableHead className="">Email</TableHead>
                             <TableHead className="">Numéro de téléphone</TableHead>
                             <TableHead className="text-right">Référence de paiment</TableHead>
